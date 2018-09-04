0ae6dda27fd53ce401ec2d5c5e33d1f1b629561e
Harekrishna Panda <38816064+harekrishnapanda@users.noreply.github.com>
2018-09-04 23:00:59 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,10 +9,12 @@ Created on Sat Sep  1 01:34:02 2018
 def data_cleaning(df):
     df.info()
     df.isnull().sum()
+    # dropping the NAN values as the count of NAN records is aroung 3/4% of total records
     df = df.dropna()
     #df.attacking_work_rate.unique()
     df.attacking_work_rate.value_counts()
     awr = ['medium', 'high','low']
+    # removing the non related categorical values as their count is vey less but might impact our model
     df = df[df['attacking_work_rate'].isin(awr)]
     df.defensive_work_rate.value_counts()
     df = df[df['defensive_work_rate'].isin(awr)]
